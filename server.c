aac58f3910bd4b7a746d0e0bfa5d7994c5d35c3d
Sergio Morales <smora015@ucr.edu>
2015-06-01 22:05:25 -0700

    Need to implement shared memory

--- server.c
+++ server.c
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
   int portno = 7158;     // Port #
   socklen_t clilen;      // Length of cli_addr struct
   char buffer[256];      // Buffer to hold messages
-
+  int n = 0;             // Used to read/write from/to socket
+      
   struct sockaddr_in serv_addr, cli_addr; // Holds address of servers and clients
-  int n;
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);  // Create a new TCP socket
   if (sockfd < 0) 
@@ -45,8 +45,8 @@ int main(int argc, char *argv[])
   listen(sockfd,5);                           
   clilen = sizeof(cli_addr);
 
-  // Set up hard coded users
-  init_users();
+  // Set up memory allocation, hard coded users, etc
+  init_data();
 
   // Start receiving messages
   printf("Successfully binded to hostname/port!\n" );
@@ -68,24 +68,49 @@ int main(int argc, char *argv[])
     }
     else if(pid == 0) // Child/Client process
     {
+
+      /*
+
+
+	MUST IMPLEMENT SHARING GLOBAL MEMORY!!!!!! 
+	http://stackoverflow.com/questions/13274786/how-to-share-memory-between-process-fork
+
+      */
+
       // Socket failed to accept to client
       if (newsockfd < 0) 
 	error("ERROR on accept");
 
       // Authenticate the user
-      if( is_authenticated( &newsockfd ) == 0 )
-	authenticate_user( &newsockfd );
-
+      authenticate_user( &newsockfd );
+      
+      // Display main menu
       current_menu = 0; // 0 = main menu, etc
+      handle_menu( &newsockfd, &current_menu ); 
 
       // Loop for handling replies and responses to/from client
       for( ;; ) 
       {	
+	// Get command from client
+	bzero(buffer,256);
+	n = read( newsockfd, buffer, 255);
+	if( n < 0) error("ERROR reading from socket");
+	current_menu = atoi( (const char*) buffer ); 
+
+	// Handle command
 	if( current_menu != 5 )
-	  // Display menu and handle commands
 	  handle_menu( &newsockfd, &current_menu );
+
+	// Log out the user if they entered '5'
 	else
 	{
+	  printf("[User %s] - Has logged off.\n", current_user->username);
+
+	  // Write to client
+	  int n = write( newsockfd,"Logged out successfully.",24);
+	  if (n < 0)
+	    error("ERROR writing to socket");
+
 	  close(newsockfd);
 	  break;
 	}
@@ -134,16 +159,47 @@ user create_user( char* username, char* password, int sockfd, message* messages)
   new_user.password = password;
   new_user.sockfd = sockfd;
   new_user.messages = messages;
+  new_user.message_count = 0;
 
   return new_user;
 }
 
-void init_users()
+user* get_current_user( int sockfd )
+{
+  user* usr = users;
+  while( usr != NULL )
+  {
+    if( usr->sockfd == sockfd)
+      return usr;
+
+    ++usr; // Iterate to next user!
+  }
+
+  return NULL;
+}
+ 
+user* get_user( char* username )
+{
+  user* usr = users;
+  while( usr != NULL )
+  {
+    if( strncmp( usr->username, username, strlen(username)) )
+	return usr;
+
+    ++usr; // Iterate to next user!
+  }
+  
+  return NULL;
+}
+
+void init_data()
 {
   // Allocate enough space in users list for 5 clients
   users = (user*) malloc( sizeof(user)*MAX_USERS );
   users_online = (user*) malloc( sizeof(user)*MAX_USERS );
-  
+  current_user = (user*) malloc( sizeof(*current_user) );
+
+
   // Hard code 3 users
   user tom = create_user( "tom", "tom_password", -1, NULL);
   user chris = create_user( "chris", "chris_password", -1, NULL);
@@ -154,25 +210,8 @@ void init_users()
   users[2] = sara;
 
   // Initialize any counters or flags
-  messagecount = 0;
+  messages_received = 0;
   
-}
-
-int is_authenticated( int* sockfd )
-{
-  // Traverse through list of users to see if socket matches any users
-  user* usr = users;
-  while( usr->username != NULL )
-  {
-    if( usr->sockfd == *sockfd )
-      return 1; // User was found, return 1
-
-    ++usr; // Iterate to next user
-  }
-
-  printf("Ending is_authenticated...\n");
-  // Else, user was not found, return 0
-  return 0;
 }
 
 // Logs in the user to post tweets
@@ -181,9 +220,11 @@ int authenticate_user( int* sockfd )
   // Message prompts
   char* un = "Username: ";
   char* ps = "Password: ";
-  char* iv = "Invalid username or password!";
+  char* iv = "Invalid username or password!\n";
   char ss[180];
   char* ss1 = "\nSuccessfully logged in! Welcome, ";
+  char* ss2 = ".\nYou have ";
+  char* ss3 = " messages.\n";
   user* usr; // Used to traverse list of users
   int found = 0;
   int n = 0;
@@ -219,7 +260,14 @@ int authenticate_user( int* sockfd )
       {
 	found = 1; // Set found flag to true
 	usr->sockfd = *sockfd; // Add current socket associated to user
-	strcat( strcat( strcat(ss, ss1), usr->username ), "\n");
+
+	strcat( strcat(ss, ss1), usr->username );        // Contantenate welcome message
+	char mc[20];
+	snprintf(mc, 20, "%d", usr->message_count);
+	strcat( strcat( strcat( ss, ss2 ), mc ), ss3);
+
+	current_user = (user*) malloc( sizeof(user) );   // Set current user
+	current_user = get_current_user( *sockfd ); 
       }
       ++usr; // Iterate to next user
     }
@@ -235,30 +283,36 @@ int authenticate_user( int* sockfd )
 
 void handle_menu( int* sockfd, int* menu )
 {
+  int n;
   char buffer[255];
+
   // Get reply from client
   if( *menu == 0 ) // Main Menu
-    {
+  {
     char* main_msg = "============================\n CS164 Twitter Clone - Main Menu \n============================\n\
-1. Offline Messages\n2. Edit Subscriptions\n 3. Post a message\n4. Hashtag search\n5. Logout (or 'quit')";
+1. Offline Messages\n2. Edit Subscriptions\n3. Post a message\n4. Hashtag search\n5. Logout (or 'quit'): ";
 
     // Display main message
     int n = write(*sockfd,main_msg,strlen(main_msg));
     if (n < 0) error("ERROR writing to socket");
 
-    n = read( *sockfd, buffer, 255);
-    if( n < 0) error("ERROR reading from socket");
-    
-    *menu = atoi( (const char*) buffer );
   }  
   else if( *menu == 1 ) // See Offline Messages
   {
+    char* main_msg = "============================\n CS164 Twitter Clone - Offline Messages \n============================\n0. Back";
 
+    if( current_user->messages == NULL )
+    {
+      char * msg = "You have don't have any offline messages.\n";
+      strcat( strcat( buffer, main_msg ), msg );
 
+      n = write( *sockfd, buffer, strlen(buffer) );
+      if( n < 0 ) error("ERROR writing to socket");
+    }    
   }
   else if( *menu == 2 ) // Edit Subscriptions
   {
-
+    
   }
   else if( *menu == 3 ) // Post a message
   {
@@ -267,15 +321,6 @@ void handle_menu( int* sockfd, int* menu )
   else if( *menu == 4) // Hashtag search
   {
 
-  }
-  else if( *menu == 5) // Logout
-  {
-    printf("Status: [Client %i] - Logged out...\n", *sockfd);
-
-    // Write to client
-    int n = write( *sockfd,"Logged out successfully.",24);
-    if (n < 0)
-      error("ERROR writing to socket");
   }
   else
   {
